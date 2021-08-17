74be9b14d8c1cb74813442bd19221e08493349ec
Higins <greczb20@gmail.com>
2021-08-17 13:46:16 +0200

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract CharityManager {
+	function performTasks() public {
+	    
+	}
+
+	function charity() public pure returns (address) {
+		return 0x5533719E8719328d5dFaADdc48f263a5cb7a538F;
+	}
+}
