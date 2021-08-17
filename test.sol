40ce4a5117c2790fedf32ff77d90b97beb4512ab
Higins <greczb20@gmail.com>
2021-08-17 14:30:43 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.5.0;
 
 contract CharityManager {
 	function charity() public pure returns (address) {
-		return 0x5533719E8719328d5dFaADdc48f263a5cb7a538F;
+		return 0x5533719E8719328d5dFaADdc48f263a5cb7a9999;
 	}
 }
